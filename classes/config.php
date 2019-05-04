66b31d85522ed69a1c925c0974dd431bf0dca28a
Wimko <jurik.matejw@gmail.com>
2019-05-04 20:11:46 +0200

    :)

--- classes/config.php
+++ classes/config.php
@@ -9,6 +9,8 @@ class Config {
     const SMTP_PORT = "2525";
     const SMTP_USER = "d4b7c47ed47c5f";
     const SMTP_PASSWORD = "90f62ded73300b";
+    // I believe nobody in their right mind will use this :}
+    // so I decided not to move it to the .env file yet
 
     /**
      * Address constants
